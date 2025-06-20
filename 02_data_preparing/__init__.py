b5ca0aeec6302e11742efbdb12b0a3709c0660b9
SIQRIT <rhys@SIQRITui-MacBookPro.local>
2025-06-20 20:55:38 +0900

    Add: __init__.py

---
+++ 02_data_preparing/__init__.py
@@ -0,0 +1,5 @@
+from 01_oauth import 01_oauth
+from 02_request import 02_request
+from 03_crawling import 03_crawling
+
+print("DAIS: https://huggingface.co/SIQRIT/DAIS-Qwen3-8B-qdora")
---
+++ 03_data_preprocessing/__init__.py
@@ -0,0 +1,8 @@
+from 01_data_deduping import 01_data_deduping
+from 02_data_refining import 02_data_refining
+from 03_data_length import 03_data_length
+from 04_data_english import 04_data_english
+from 05_data_api import 05_data_api
+from 06_data_preprocessing import 06_data_preprocessing
+
+print("DAIS: https://huggingface.co/SIQRIT/DAIS-Qwen3-8B-qdora")
